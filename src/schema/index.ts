4794c477c6ca3916e3e3704fa794592f6f526dd8
christopher-caldwell <well.done.514@gmail.com>
2024-08-17 10:05:11 -0500

    feat: Putting barrel file in place

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,7 +1,16 @@
 import './actor'
+import './address'
 import './category'
+import './city'
+import './country'
+import './customer'
 import './film'
+import './inventory'
+import './language'
+import './payment'
+import './rental'
 import './staff'
+import './store'
 
 import { builder } from './builder'
 
